4b2484723a4a676d5b3a222db65d4b331b13daa6
ionoy <mihhail.maslakov@gmail.com>
2017-06-12 13:28:18 +0300

    added c# 6 test

---
+++ test/Ammy.Test.Wpf/CSharp/CSharp6Test.ammy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ammy.WpfTest.CSharp
+{
+    public partial class CSharp6Test
+    {
+        public CSharp6Test()
+        {
+            InitializeComponent();
+        }
+    }
+}
